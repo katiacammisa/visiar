e8f3b535a5b0e970c8935e50fa37ee76f201cc20
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2022-10-17 15:19:29 -0300

    tp final avanzado

--- tp_final/detector.py
+++ tp_final/detector.py
@@ -1,8 +1,10 @@
 import cv2
+import numpy as np
 
 from contour import get_contours, get_poke_contours
 from frame_editor import apply_color_convertion, denoise
 from details import getPokemonData
+from test import getPokeContours
 from trackbar import create_trackbar, get_trackbar_value
 
 
@@ -17,57 +19,36 @@ def main():
     color_green = (0, 255, 0)
     create_trackbar(trackbar_name, window_name, 1, slider_max)
     create_trackbar(trackbar_name2, window_name, 1, 50)
-    detected_contours = []
-    saved_contours = []
-    pokeDict = getPokemonData()
 
-    for n in range(1, 2):
-        img = cv2.imread(f'./images/{n}.png')
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        ret2, thresh2 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        contoursPoke, hierarchy = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        # cv2.drawContours(imGris, contoursPoke[20], -1, color_green, 20)
-        saved_contours.append(contoursPoke[20])
+    pokeDict = getPokemonData()
+    pokeContours = getPokeContours(pokeDict)
 
     while True:
         ret, frame = cap.read()
-        gray_frame = apply_color_convertion(frame=frame, color=cv2.COLOR_RGB2GRAY)  # Hace la imagen gris
-        trackbar_val = get_trackbar_value(trackbar_name=trackbar_name, window_name=window_name)
-        ret10, adapt_frame = cv2.threshold(gray_frame, trackbar_val, slider_max, cv2.THRESH_BINARY)
-        trackbar_val2 = get_trackbar_value(trackbar_name=trackbar_name2, window_name=window_name)
-        frame_denoised = denoise(frame=adapt_frame, method=cv2.MORPH_ELLIPSE, radius=trackbar_val2)
-
-        contours = get_contours(frame=frame_denoised, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        gray_frame = apply_color_convertion(frame=frame, color=cv2.COLOR_RGB2GRAY)
+        contours = get_contours(frame=gray_frame, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
         filtered = []
+
         for c in contours:
-            if 7000 < cv2.contourArea(c) < 50000:
+            if 30000 < cv2.contourArea(c):
                 filtered.append(c)
 
-        if len(filtered) < 1:
-            print("filtered is empty")
-
-        imGris = cv2.cvtColor(frame_denoised, cv2.COLOR_GRAY2RGB)
+        imGris = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2RGB)
 
         if len(filtered) > 0:
 
             for f in filtered:
 
-                if cv2.waitKey(1) & 0xFF == ord('k'):
-                    if f is not None:
-                        detected_contours.append(f)
-
-                for index in range(0, len(saved_contours)):
-                    if cv2.matchShapes(f, saved_contours[index], cv2.CONTOURS_MATCH_I2, 0) < 0.2:
+                for index in range(len(pokeContours)):
+                    if cv2.matchShapes(f, pokeContours[index], cv2.CONTOURS_MATCH_I2, 0) < 0.4:
                         x, y, w, h = cv2.boundingRect(f)
-                        # cv2.rectangle(imGris, (x, y), (x + w, y + h), color_green, 20)
-                        # cv2.putText(imGris, '', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_green, 2)
                         cv2.putText(imGris, pokeDict.get(index).name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_green, 2)
-                        cv2.drawContours(imGris, contours, -1, color_green, 20)
+                        cv2.drawContours(imGris, f, -1, color_green, 20)
 
-                else:
-                    x, y, w, h = cv2.boundingRect(f)
-                    cv2.rectangle(imGris, (x, y), (x + w, y + h), color_red, 20)
-                    cv2.putText(imGris, 'Unidentified', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_red, 2)
+                    # else:
+                    #     x, y, w, h = cv2.boundingRect(f)
+                    #     cv2.rectangle(imGris, (x, y), (x + w, y + h), color_red, 20)
+                    #     cv2.putText(imGris, 'Unidentified', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_red, 2)
 
         cv2.imshow('Tp Final', imGris)
 
--- tp_final/test.py
+++ tp_final/test.py
@@ -1,39 +1,29 @@
 import cv2 as cv
-import pandas as pd
+import numpy as np
 
-from scrapper import getFromUrl
-from trackbar import get_trackbar_value, create_trackbar
+from scrapper import saveImageFromUrl
 from details import getPokemonData
 
-def main():
-    pokeDict = getPokemonData()
-    n = 2
-    image = pokeDict.get(n).image
-    getFromUrl(n,image)
-    image2 = cv.imread(f'./images/{n}.png')
-    gray = cv.cvtColor(image2, cv.COLOR_RGB2GRAY)
-    ret1, thresh1 = cv.threshold(gray, 127, 255, cv.THRESH_BINARY_INV)
-    contours, hierarchy = cv.findContours(thresh1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-    cv.drawContours(image2, contours, -1, (0, 0, 255), 20)
-    cv.imshow("blah", image2)
-    cv.waitKey()
-    # contours = [cnt1, cnt2, cnt3]
-    
 
-    window_name = 'Tp Final'
-    trackbar_name = 'Threshold'
-    cv.namedWindow(window_name)
-    create_trackbar(trackbar_name, window_name, 0, 100)
-    counter = 0
-    for cont in contours:
-        trackbar_value = get_trackbar_value(trackbar_name=trackbar_name, window_name=window_name)
+def getPokeContours(pokeDict):
+    # pokeDict = getPokemonData()
+    list = []
+    for n in range(2):
+        url = pokeDict.get(n).image
+        saveImageFromUrl(n, url)
+        image = cv.imread(f'./images/{n}.png')
+        gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
 
-        cv.drawContours(gray,cont, -1, (0, 0, 255), 3)
-        # cv.drawContours(gray, contours[0], -1, (0, 0, 255), 3)
-        cv.imshow(window_name, gray)
-        print(counter)
-        counter = counter+1
+        # TODO Le setteamos un valor a la trackbar
+        ret1, thresh1 = cv.threshold(gray, 127, 255, cv.THRESH_BINARY_INV)
+        contours, hierarchy = cv.findContours(thresh1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-        cv.waitKey()
-
-main()
+        # Saving the array in a text file
+        # np.savetxt(f'./pokeContours/{n}.csv', contours[0], delimiter=',')
+        # file = open(f'./pokeContours/{n}.txt', 'w+')
+        # content = contours[0]
+        # file.write(content)
+        # file.close()
+        # TODO NOS PONE LOS 3 PUNTITOSSSS
+        list.append(contours[0])
+    return list

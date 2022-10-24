0d7ddc22c417cd3238cdcd3936f632918a369e46
Alejo Ramirez Gismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2022-10-24 14:49:27 -0300

    ROI from frame

--- tp_final/detector.py
+++ tp_final/detector.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-from contour import get_contours, get_poke_contours
-from frame_editor import apply_color_convertion, denoise
+from contour import get_contours
 from details import getPokemonData
+from frame_editor import apply_color_convertion
 from test import getPokeContours
-from trackbar import create_trackbar, get_trackbar_value
+from trackbar import create_trackbar
 
 
 def main():
@@ -25,8 +25,14 @@ def main():
 
     while True:
         ret, frame = cap.read()
+
+        # Cuts square in the middle of the screen to only detect contours in there
+        width = int(cap.get(3))  # float `width`
+        height = int(cap.get(4))  # float `height`
+        ROI = frame[int(height * 0.3):int(height * 0.7), int(width * 0.3):int(width * 0.7)]
+
         colors = frame.copy()
-        gray_frame = apply_color_convertion(frame=frame, color=cv2.COLOR_RGB2GRAY)
+        gray_frame = apply_color_convertion(frame=ROI, color=cv2.COLOR_RGB2GRAY)
         ret1, thresh1 = cv2.threshold(gray_frame, 127, 255, cv2.THRESH_BINARY_INV)
         contours = get_contours(frame=thresh1, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
         filtered = []
@@ -44,8 +50,8 @@ def main():
                         x, y, w, h = cv2.boundingRect(f)
                         text = getPokeData(pokeDict.get(index))
                         for i, line in enumerate(text.split('\n')):
-                            y1 = y + i*50
-                            cv2.putText(colors, line, (x+w+10, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_green, 2)
+                            y1 = y + i * 50
+                            cv2.putText(colors, line, (x + w + 10, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color_green, 2)
                         cv2.drawContours(colors, f, -1, color_green, 20)
 
                     # else:
